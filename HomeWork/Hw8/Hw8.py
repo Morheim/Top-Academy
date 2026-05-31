4cef02b63435497862b1f48e76028707b76d96a2
Oleg Sheliuta <Morheim19970821_11@mail.ru>
2026-06-01 00:19:17 +0400

    add

--- HomeWork/Hw8/Hw8.py
+++ HomeWork/Hw8/Hw8.py
@@ -1,9 +1,39 @@
-def greatest_common_divisor(val1, val2):
-    if val2 == 0:
-        return val1
-    else:
-        print(f"val1 ({val1}) | ({val2}) val2\n")
-        return greatest_common_divisor(val2, val1 % val2)
+def main():
+    tasks = ["Сделать домашнюю работу", "Помыть посуду", "Прочитать книгу"]
 
-print(greatest_common_divisor(int(input(f"val1 = ")), int(input(f"val2 = "))))
+    task_iter = iter(tasks)
 
+    completed_count = 0
+
+    print("Добро пожаловать в менеджер задач!")
+    print("Доступные команды: next, list, exit\n")
+
+    while True:
+        command = input("Введите команду > ").strip().lower()
+
+        if command == "next":
+            try:
+                current_task = next(task_iter)
+                print(f"Текущая задача: {current_task}")
+                completed_count += 1
+            except StopIteration:
+                print("Все задачи выполнены!")
+
+        elif command == "list":
+            if completed_count < len(tasks):
+                print("Оставшиеся задачи:")
+                for task in tasks[completed_count:]:
+                    print(f"\t*|{task}")
+            else:
+                print("Список задач пуст. Все задачи уже выполнены!")
+
+        elif command == "exit":
+            print("Программа завершена. До свидания!")
+            break
+
+        else:
+            print("Неизвестная команда. Введите 'next', 'list' или 'exit'.")
+
+
+if __name__ == "__main__":
+    main()
